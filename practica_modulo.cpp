45e38336621f26df26711de4301bd68d0a210d65
benjamin <benjamingutiterrez89@gmail.com>
2024-06-06 12:19:36 +0200

    agregando contenido

--- practica_modulo.cpp
+++ practica_modulo.cpp
@@ -12,6 +12,13 @@ int random_n()
     n = 1 + rand() % 3;
     return n;
 }
+int random_notas()
+{
+    int n;
+
+    n = 1 + rand() % 10;
+    return n;
+}
 
 using namespace std;
 struct notas
@@ -31,26 +38,43 @@ void mostrar(vector<int> identificacion, vector<notas> &datos, vector<string> no
 
     for (int i = 0; i < 10; i++)
     {
-        grupo = "GInf" + to_string(random_n());
+        if (datos[i].nombre_a == "" && datos[i].NumAlumno == 0 && datos[i].Grupo == "" && datos[i].teoría == 0 && datos[i].problemas == 0 && datos[i].prácticas == 0)
+        {
+            grupo = "GInf" + to_string(random_n());
 
-        datos[i].nombre_a = nombres[i];
-        datos[i].NumAlumno = identificacion[i];
-        datos[i].Grupo = grupo;
-        datos[i].teoría = random_n();
-        datos[i].problemas = random_n();
-        datos[i].prácticas = random_n();
+            datos[i].nombre_a = nombres[i];
+            datos[i].NumAlumno = identificacion[i];
+            datos[i].Grupo = grupo;
+            datos[i].teoría = random_notas();
+            datos[i].problemas = random_notas();
+            datos[i].prácticas = random_notas();
 
-        cout << "-----------------------------------------\n";
-        cout << "-----------------------------------------\n";
-        cout << "*******" << datos[i].nombre_a << "*******\n";
-        cout << "-----------------------------------------\n";
-        cout << "-----------------------------------------\n";
-        cout << "EL alumno numero :" << datos[i].NumAlumno << endl;
-        cout << "EL Grupo :" << datos[i].Grupo << endl;
-        cout << "La nota de Teoria :" << datos[i].teoría << endl;
-        cout << "La nota de Problemas :" << datos[i].problemas << endl;
-        cout << "La nota de Practicas:" << datos[i].prácticas << endl;
-        cout << "\n";
+            cout << "-----------------------------------------\n";
+            cout << "-----------------------------------------\n";
+            cout << "*******" << datos[i].nombre_a << "*******\n";
+            cout << "-----------------------------------------\n";
+            cout << "-----------------------------------------\n";
+            cout << "EL alumno numero :" << datos[i].NumAlumno << endl;
+            cout << "EL Grupo :" << datos[i].Grupo << endl;
+            cout << "La nota de Teoria :" << datos[i].teoría << endl;
+            cout << "La nota de Problemas :" << datos[i].problemas << endl;
+            cout << "La nota de Practicas:" << datos[i].prácticas << endl;
+            cout << "\n";
+        }
+        else
+        {
+            cout << "-----------------------------------------\n";
+            cout << "-----------------------------------------\n";
+            cout << "*******" << datos[i].nombre_a << "*******\n";
+            cout << "-----------------------------------------\n";
+            cout << "-----------------------------------------\n";
+            cout << "EL alumno numero :" << datos[i].NumAlumno << endl;
+            cout << "EL Grupo :" << datos[i].Grupo << endl;
+            cout << "La nota de Teoria :" << datos[i].teoría << endl;
+            cout << "La nota de Problemas :" << datos[i].problemas << endl;
+            cout << "La nota de Practicas:" << datos[i].prácticas << endl;
+            cout << "\n";
+        }
     }
 }
 
@@ -166,11 +190,77 @@ void merjornota(vector<notas> datos)
         }
     }
 }
+void notamedia(vector<notas> datos)
+{
+    int nota = 0;
+    int alumno;
+    cout << "introduzca el numero del Alumnos del cual desea saber la nota media:\n";
+    cin >> alumno;
+    nota = (datos[alumno - 1].teoría + datos[alumno - 1].problemas + datos[alumno - 1].prácticas) / 3;
+
+    cout << "la nota media es: \n"
+         << nota;
+}
+
+void suspensos(vector<notas> datos)
+{
+
+    cout << "-----------------------------------------\n";
+    cout << "---------Alumnos suspensos---------------\n";
+    cout << "-----------------------------------------\n";
+
+    for (int i = 0; i < 10; i++)
+    {
+
+        if (datos[i].teoría < 5 || datos[i].problemas < 5 || datos[i].prácticas < 5)
+        {
+            cout << "*******" << datos[i].nombre_a << "*******\n";
+            cout << "\n";
+        }
+    }
+}
+
+void suspensosgrupo(vector<notas> datos)
+{
+    int GInf1 = 0;
+    int GInf2 = 0;
+    int GInf3 = 0;
+
+    cout << "-----------------------------------------\n";
+    cout << "------Alumnos suspensos por grupos-------\n";
+    cout << "-----------------------------------------\n";
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (datos[i].Grupo == "GInf1" && (datos[i].teoría < 5 || datos[i].problemas < 5 || datos[i].prácticas < 5))
+        {
+            GInf1++;
+            GInf1 = +GInf1;
+        }
+        if (datos[i].Grupo == "GInf2" && (datos[i].teoría < 5 || datos[i].problemas < 5 || datos[i].prácticas < 5))
+        {
+            GInf2++;
+            GInf2 = +GInf2;
+        }
+        if (datos[i].Grupo == "GInf3" && (datos[i].teoría < 5 || datos[i].problemas < 5 || datos[i].prácticas < 5))
+        {
+            GInf3++;
+            GInf3 = +GInf3;
+        }
+    }
+    cout << "\n";
+    cout << "El numero de alumnos suspensos del grupo GInf1 es: " << GInf1 << endl;
+    cout << "\n";
+    cout << "El numero de alumnos suspensos del grupo GInf2 es: " << GInf2 << endl;
+    cout << "\n";
+    cout << "El numero de alumnos suspensos del grupo GInf3 es: " << GInf3;
+    cout << "\n";
+}
 void submenu(vector<int> identificacion, vector<notas> datos, vector<string> nombres)
 {
     int menu;
     void menuprincipal(vector<int> & identificacion, vector<notas> datos, vector<string> nombres);
-
+    void mostrar(vector<int> identificacion, vector<notas> & datos, vector<string> nombres);
     cout << "\n";
     cout << "     SUBMENU DE ESTADISTICAS\n";
     cout << "********************************\n";
@@ -202,13 +292,20 @@ void submenu(vector<int> identificacion, vector<notas> datos, vector<string> nom
         submenu(identificacion, datos, nombres);
         break;
     case 2:
-
+        system("clear");
+        notamedia(datos);
+        submenu(identificacion, datos, nombres);
         break;
     case 3:
-
+        system("clear");
+        suspensos(datos);
+        submenu(identificacion, datos, nombres);
         break;
     case 4:
-
+        system("clear");
+        suspensosgrupo(datos);
+        submenu(identificacion, datos, nombres);
+        ;
         break;
     default:
         submenu(identificacion, datos, nombres);
